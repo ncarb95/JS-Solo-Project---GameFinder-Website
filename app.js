f52fe7f18f9f235f66284c23863dd86757cf5c41
Nick Carbone <nickryancarbone@gmail.com>
2026-05-31 20:17:11 -0500

    added try/catch blocks

--- app.js
+++ app.js
@@ -11,48 +11,54 @@ async function fetchGames(query = null) {
 }
 
 async function renderGames(query = null) {
-  const gameCardsList = document.querySelector("#game__cards__list");
-  const gameNameInput = document.querySelector('.header__text__search__input');
+  try {
+    const gameCardsList = document.querySelector("#game__cards__list");
+    const gameNameInput = document.querySelector(
+      ".header__text__search__input",
+    );
 
-  gameCardsList.innerHTML = `<i class="fa-solid fa-circle-notch loading__spinner"></i>`;
-  gameNameInput.value = '';
+    gameCardsList.innerHTML = `<i class="fa-solid fa-circle-notch loading__spinner"></i>`;
+    gameNameInput.value = "";
 
-  const games = await fetchGames(query);
+    const games = await fetchGames(query);
 
-  const gameCardsListHTML = games
-    .map((game) => {
-      return `<div class="game__card">
-                            <div class="game__card__img__wrapper">
-                                <a href="#" class="game__card__img__link">
-                                    <img src="${game.background_image}" alt="${game.name}" class="game__card__img">
-                                </a>
-                            </div>
-                            <div class="game__card__text">
-                                <div class="game__card__main">
-                                    <a href="#" class="game__card__title">
-                                        ${game.name}
-                                    </a>
-                                </div>
-                                <div class="game__card__sub">
-                                    <span class="game__card__rating">${game.rating > 0 ? game.rating + " / 5" : "N/A"}</span>
-                                </div>
-                            </div>
-                        </div>`;
-    })
-    .join("");
+    const gameCardsListHTML = games
+      .map((game) => {
+        return `<div class="game__card">
+                              <div class="game__card__img__wrapper">
+                                  <a href="#" class="game__card__img__link">
+                                      <img src="${game.background_image}" alt="${game.name}" class="game__card__img">
+                                  </a>
+                              </div>
+                              <div class="game__card__text">
+                                  <div class="game__card__main">
+                                      <a href="#" class="game__card__title">
+                                          ${game.name}
+                                      </a>
+                                  </div>
+                                  <div class="game__card__sub">
+                                      <span class="game__card__rating">${game.rating > 0 ? game.rating + " / 5" : "N/A"}</span>
+                                  </div>
+                              </div>
+                          </div>`;
+      })
+      .join("");
 
-  gameCardsList.innerHTML = gameCardsListHTML;
+    gameCardsList.innerHTML = gameCardsListHTML;
+  } catch(error) {
+    alert(error);
+  }
 }
 
 function searchGames() {
-  const headerInput = document.querySelector('#header__game__name');
-  const navInput = document.querySelector('#nav__game__name');
+  const headerInput = document.querySelector("#header__game__name");
+  const navInput = document.querySelector("#nav__game__name");
   const query = (headerInput.value || navInput.value).trim();
 
   if (!query) return;
 
-  headerInput.value = '';
-  navInput.value = '';
+  headerInput.value = "";
+  navInput.value = "";
   renderGames(query);
 }
 
